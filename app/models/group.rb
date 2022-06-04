935843d9a7dc5e8c69895b269751ed02c6a85777
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-05 03:31:44 +0500

    :ambulance: reconfiguring

--- app/models/group.rb
+++
@@ -1,8 +0,0 @@
-class Group < ApplicationRecord
-  belongs_to :user
-  has_many :group_entities, dependent: :destroy
-  has_many :entities, through: :group_entities, dependent: :destroy
-
-  validates :name, presence: true, length: { maximum: 250 }
-  validates :icon, presence: true
-end
