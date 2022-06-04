d9117b98d0c867c3eb1888c6581128807ff7cb29
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-05 01:52:38 +0500

    :lock: added validations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,8 @@ class Entity < ApplicationRecord
   belongs_to :user
   has_many :group_entities, dependent: :destroy
   has_many :groups, through: :group_entities, dependent: :destroy
+
+  validates :name, presence: true
+  validates :amount, presence: true
+  validates :user_id, presence: true
 end
