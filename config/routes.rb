c1a0ca1a730ec56ad397a03ca642348e73d03c03
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-05 05:27:06 +0500

    :art: added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'categories#index'
+  devise_for :users
   resources :categories, only: [:index, :new, :create] do
     resources :transactions, only: [:index, :new, :create]
   end
