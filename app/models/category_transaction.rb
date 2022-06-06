fc64e6fd54054868b9586ca2b40ac3d5eb272152
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-06 15:15:14 +0500

    :art: added views

--- app/models/category_transaction.rb
+++
@@ -1,2 +0,0 @@
-class CategoryTransaction < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_05_001600) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_092358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,21 +23,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_05_001600) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table "category_transactions", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "category_id", null: false
-    t.bigint "transaction_id", null: false
-    t.index ["category_id"], name: "index_category_transactions_on_category_id"
-    t.index ["transaction_id"], name: "index_category_transactions_on_transaction_id"
-  end
-
   create_table "transactions", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
@@ -59,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_05_001600) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "category_transactions", "categories"
-  add_foreign_key "category_transactions", "transactions"
+  add_foreign_key "transactions", "categories"
   add_foreign_key "transactions", "users"
 end
